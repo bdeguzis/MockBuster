e06fa913f437eaecc15c3d06ca4a147cc54ba6fa
bdeguzis <b.deguzis@gmail.com>
2019-02-18 15:53:55 -0500

    Add project files.

---
+++ MockBuster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MockBuster.Startup))]
+namespace MockBuster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
